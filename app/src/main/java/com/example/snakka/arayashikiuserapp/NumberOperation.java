9c135a6e448dfe65b1e7c0db846ad819a86220b8
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2017-06-26 15:48:22 +0900

    NumberOperationの変数の形式を変更、public finalの変数を追加
    方角ごとにint型変数を宣言してたが、int[]型にまとめて格納に変更
    方角事の定数public finalの変数を作成
    進路の定数public finaの作成l

--- app/src/main/java/com/example/snakka/arayashikiuserapp/NumberOperation.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/NumberOperation.java
@@ -5,94 +5,142 @@ package com.example.snakka.arayashikiuserapp;
  * ユーザー側のナンバー操作クラス
  */
 
-public class Number {
+public class NumberOperation {
+
+    public static final int FRONT = 1;
+    public static final int RIGHT = 2;
+    public static final int LEFT = 3;
+    public static final int END = 0;
+
+    private final int NORTH = 0;
+    private final int EAST = 1;
+    private final int SOUTH = 2;
+    private final int WEST = 3;
+
+    private int[]  drectionNumber = new int[4]; //どの方角に進路があるかを格納：0 無  -1 backNum
 
     private int currentNum; //現在地のNumber
-    private int nextNorth, nextSouth,  //進路方向にある次のNumber北南西東
-            nextWest, nextEast;
+    /*private int nextNorth, nextSouth,  //進路方向にある次のNumber北南西東
+            nextWest, nextEast;*/
     private int backNum;    //一つ前に通り過ぎたNumber
 
     //コンストラクターの宣言（初期化
-    public Number (){
+    public NumberOperation (){
         currentNum = 0;
-        nextNorth = 0;// N北
-        nextSouth = 0;// S南
-        nextWest = 0;// W西
+        /*nextNorth = 0;// N北
         nextEast = 0;// E東
+        nextSouth = 0;// S南
+        nextWest = 0;// W西*/
         backNum = 0;
+        for(int i = 0 ; i < drectionNumber.length ; i++){
+            drectionNumber[i] = 0;
+        }
     }
 
 
-    //setNum:サーバーから送られる情報をセットする
+    //setNum:センサーから送られる情報をセットする
     /*引数
-    num ラズパイから取得したNumber*/
-    public void SetNum(int num){
+    num センサーから取得したNumber*/
+    public void setNum(int num){
         backNum = currentNum;
         currentNum = num;
 
     }
 
+    //TODO:setnext[方角]:サーバーから送られてくる四方のNumberをdrectionNumber[方角]にセットする
+    /*引数
+    next[方角] 方角事のNumber センサーが無ければ：0 有れば：非0
+     */
     public void setNextNorth(int nextNorth) {
-        if (nextNorth == backNum) {
-            this.nextNorth = -1;
+        if (nextNorth == backNum) { //backNumとnext
+            drectionNumber[NORTH] = -1;
         }else{
-        this.nextNorth = nextNorth;
-        }
-    }
-
-    public void setNextSouth(int nextSouth) {
-        if (nextSouth == backNum) {
-            this.nextSouth = -1;
-        }else {
-            this.nextSouth = nextSouth;
-        }
-    }
-
-    public void setNextWest(int nextWest) {
-        if (nextWest == backNum) {
-            this.nextWest = -1;
-        }else {
-            this.nextWest = nextWest;
+            drectionNumber[NORTH] = nextNorth;
         }
     }
 
     public void setNextEast(int nextEast) {
         if (nextEast == backNum) {
-            this.nextEast = -1;
+            drectionNumber[EAST] = -1;
         }else {
-            this.nextEast = nextEast;
+            drectionNumber[EAST] = nextEast;
         }
     }
 
+    public void setNextSouth(int nextSouth) {
+        if (nextSouth == backNum) {
+            drectionNumber[SOUTH] = -1;
+        }else {
+            drectionNumber[SOUTH] = nextSouth;
+        }
+    }
 
+    public void setNextWest(int nextWest) {
+        if (nextWest == backNum) {
+            drectionNumber[WEST] = -1;
+        }else {
+            drectionNumber[WEST] = nextWest;
+        }
+    }
 
+    //TODO:現在地のナンバーを取得 必要性は薄いかもしれない
     public int getCurrentNum() {
         return currentNum;
     }
-
-    public int getBackNumet(){
+    //来た方向のNumberを取得
+    public int getBackNum(){
         return backNum;
     }
 
-
     //GetNext:進路の先にあるNumberを取得する
     public int getNextNorth() {
-        return nextNorth;
+        return drectionNumber[NORTH];
     }
 
     public int getNextSouth() {
-        return nextSouth;
+        return drectionNumber[SOUTH];
     }
 
     public int getNextWest() {
-        return nextWest;
+        return drectionNumber[WEST];
     }
 
     public int getNextEast() {
-        return nextEast;
+        return drectionNumber[EAST];
     }
 
-    public String cours() {
 
+    private int[] getSelectCourse(int backDrection){
+        int[] courseSelect = new int[4];
+        int courseSelectIdx = 0;
+        int routeDrection = backDrection;
+
+        /*routeDrectionが現在backDrection(後ろ)を示しているのに対して、
+        進路の前を示すように変更する
+        例：backが SOUTH == 2 の時は、前は NORTH == 0 */
+        if ((routeDrection += 2 ) > 3 ){
+            routeDrection -= 4;
+        }
+
+
+        setCourse: for(int selectCourse = 1 ; selectCourse <= 3 ; ){
+            if(routeDrection == backDrection){
+                continue;
+            }
+            if()
+            drectionNumber[routeDrection]
+
+            if((routeDrection + 1) <= 3){
+                routeDrection += 1;
+            }
+        }
+    }
+
+    public int[] getCourse() {
+        int backDrection = 0;
+
+        for (; drectionNumber[backDrection] == backNum; backDrection++) {}
+
+        return getSelectCourse(backDrection);
     }
 }
