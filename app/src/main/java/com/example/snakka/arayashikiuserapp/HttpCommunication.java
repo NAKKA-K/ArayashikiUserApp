f5c59d1ca48edb09a7168d187208d864569d2063
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2017-07-10 12:34:44 +0900

    httpGetServerメゾット作成

---
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/HttpCommunication.java
@@ -0,0 +1,16 @@
+package com.example.snakka.arayashikiuserapp;
+
+/**
+ * Created by morikei on 2017/07/03.
+ */
+
+public class HttpCommunication {
+    public void httpGetServer{
+
+    }
+    public void httpGetNum{
+        new Thread(new Runnable() {
+
+        }
+    }
+}
