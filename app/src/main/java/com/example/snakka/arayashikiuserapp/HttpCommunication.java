541b8b9c2d972a474b58272f5850ca5642a66045
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2017-07-21 10:54:58 +0900

    HttpCommunicationの仮完成
    テストがまだ終わってないのでバグは色々でます

--- app/src/main/java/com/example/snakka/arayashikiuserapp/HttpCommunication.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/HttpCommunication.java
@@ -1,6 +1,10 @@
 package com.example.snakka.arayashikiuserapp;
 
+import android.os.AsyncTask;
 import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,10 +18,21 @@ import java.net.URL;
  * Created by morikei on 2017/07/03.
  */
 
-public class HttpCommunication extends AsyncTask<void, void, String> {
-    private static final String JSONURL = "";
+public class HttpCommunication extends AsyncTask<Void, Void, String> {
+    private static final String JSONURL = "/mana/";
 
-    protected String doInBackground() {
+    private String block = "false";
+
+    private String fourWayNomberN = "null";
+    private String fourWayNomberS = "null";
+    private String fourWayNomberW = "null";
+    private String fourWayNomberE = "null";
+
+    private String trafficLightAddress = "";
+    //TODO:信号機の状態今のところ実装なし
+    //private String trafficLightSignal = "false";
+
+    protected String doInBackground(Void...v) {
         String resultJson = "";
         HttpURLConnection getCon = null;
         try {
@@ -58,6 +73,46 @@ public class HttpCommunication extends AsyncTask<void, void, String> {
 
     /* doInBackgroundの処理が終わった後で処理を開始する
         ・Jsonの取り扱いを行う*/
-    protected void onPostExecute (String jsonStr)
+    protected void onPostExecute (String json){
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            block = jsonObject.getString("type");
+            fourWayNomberN = jsonObject.getJSONObject("FourWayNomber").getString("nothNo");
+            fourWayNomberS = jsonObject.getJSONObject("FourWayNomber").getString("southNo");
+            fourWayNomberW = jsonObject.getJSONObject("FourWayNomber").getString("westNo");
+            fourWayNomberE = jsonObject.getJSONObject("FourWayNomber").getString("eastNo");
+            trafficLightAddress = jsonObject.getJSONObject("TrafficLight").getString("address");
+            //trafficLightSignal = jsonObject.getJSONObject("TrafficLight").getString("signal");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public boolean getBlock() {
+        return Boolean.parseBoolean(block);
+    }
+
+    public int getFourWayNomberN() {
+        return Integer.parseInt(fourWayNomberN);
+    }
+
+    public int getFourWayNomberS() {
+        return Integer.parseInt(fourWayNomberS);
+    }
+
+    public int getFourWayNomberW() {
+        return Integer.parseInt(fourWayNomberW);
+    }
+
+    public int getFourWayNomberE() {
+        return Integer.parseInt(fourWayNomberE);
+    }
+
+    public String getTrafficLightAddress() {
+        return trafficLightAddress;
+    }
+
+    /*public boolean getTrafficLightSignal(){
+        return Boolean.parseBoolean(trafficLightSignal);
+    }*/
 }
