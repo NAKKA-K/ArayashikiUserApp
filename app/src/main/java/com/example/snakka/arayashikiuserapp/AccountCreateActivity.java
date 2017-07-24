eff52624ade2315bdf487b9e1bc0b398e7ad53dc
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-07-24 16:08:28 +0900

    非同期通信を待機する処理をなくし、自動で裏で処理実行されるように変更

--- app/src/main/java/com/example/snakka/arayashikiuserapp/AccountCreateActivity.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/AccountCreateActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AccountCreateActivity extends AppCompatActivity {
     EditText nameEdit;
@@ -19,26 +20,28 @@ public class AccountCreateActivity extends AppCompatActivity {
     }
 
 
-    //TODO:テスト実装
     /*createButtonを押された時の動作
      *入力されたデータを解析して問題が無ければ登録後、スタート画面に戻る*/
     public void onCreateButtonClick(View view){
-        AccountManager accountMgr = new AccountManager();
+//        String userName = nameEdit.getText().toString();
+//        String guardianMail = mailEdit.getText().toString();
+        String userName = "tester";
+        String guardianMail = "kd1208740@st.kobedenshi.ac.jp";
 
-        String userName = nameEdit.getText().toString();
-        String guardianMail = mailEdit.getText().toString();
 
-        //TODO:受け付けない形式の文字列の場合、警告だけ表示して画面は変わらない
+        AccountManager accountMgr = new AccountManager(this, userName, guardianMail);
+
+
+        //受け付けない形式の文字列の場合、警告だけ表示して画面は変わらない
         if(accountMgr.isLogicalCheckName(userName) == false){
+            Toast.makeText(this, "使用できない文字が使われています", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
         //TODO:サーバに問い合わせて、ユーザー情報の確認とログイン。
-        if(accountMgr.postAccountToServer(userName, guardianMail)){
-            accountMgr.loginAccount(view.getContext(), userName, guardianMail);
-        }
+        accountMgr.postAccountToServer(userName, guardianMail);
 
-        finish();
     }
+
 }
