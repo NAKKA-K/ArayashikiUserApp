3a4a36e1e26e7547aa99a153f0b5f4c5378b3d57
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-07-24 17:38:03 +0900

    HTTPのPOSTを実装。ただし400or500のエラーコードが返ってくる

--- app/src/main/java/com/example/snakka/arayashikiuserapp/AccountManager.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/AccountManager.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.json.JSONObject;
 
@@ -16,19 +17,34 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class AccountManager extends Thread{
+public class AccountManager{
+    private static Context accountCreateContext;
     private static String userName;
     private static String guardianMail;
     private static final String USER_NAME_KEY = "user_name_key";
     private static final String GUARDIAN_MAIL_KEY = "guardian_mail_key";
-    private static final String URL = "http://59.106.210.231/mana/userCreate";
+
+    //private static final String URL = "http://59.106.210.231:3000/mana/userCreate";
+    private static final String PROTOCOL = "http";
+    private static final String HOST = "59.106.210.231";
+    private static final int PORT = 3000;
+    private static final String FILEPATH = "mana/userCleate/";
+
 
     private static boolean postResCode = false;
     private static String postResStr;
     private static String userJson;
+
+    public AccountManager(Context context, final String userName, final String guardianMail){
+        accountCreateContext = context;
+        this.userName = userName;
+        this.guardianMail = guardianMail;
+    }
+
 
     /** アプリにログイン情報が残っているか */
     public static boolean loginedAccount(Context context){
@@ -46,7 +62,7 @@ public class AccountManager extends Thread{
     /** 入力されたユーザー名の論理チェック */
     public boolean isLogicalCheckName(String userName){
         //TODO:userNameに使ってはいけない文字が入っていないか検出。OKならtrue
-        Pattern pattern = Pattern.compile(""); //弾くべき文字パターン
+        Pattern pattern = Pattern.compile("[ .,]"); //弾くべき文字パターン
         Matcher matcher = pattern.matcher(userName);
 
         if(matcher.find()){
@@ -58,69 +74,91 @@ public class AccountManager extends Thread{
 
 
     /** サーバにアカウント登録のPOSTをする */
-    public boolean postAccountToServer(final String userName, final String guardianMail){
-        AsyncTask<Void, Void, Integer> test = new AsyncTask<Void, Void, Integer>() {
+    public void postAccountToServer(final String userName, final String guardianMail){
+        postResCode = false; //初期化
+
+        new AsyncTask<Void, Void, Boolean>() {
             @Override
-            protected Integer doInBackground(Void... params) {
-                PrintStream outputServer = null;
+            protected Boolean doInBackground(Void... params) {
+                PrintWriter outputServer = null;
                 HttpURLConnection httpConnector = null;
                 int resCode = 0;
 
-                try{
-                    URL url = new URL(URL);
+                Log.d("HTTP:", "doInBackground通過");
+                try {
+                    //URL url = new URL(URL + getUserJson(userName, guardianMail));
+                    URL url = new URL(PROTOCOL, HOST, PORT, FILEPATH);
                     httpConnector = (HttpURLConnection) url.openConnection();
                     httpConnector.setRequestMethod("POST");
-                    httpConnector.setRequestProperty("Accept-Language", "jp");
+                    httpConnector.setDoOutput(true);
+                    httpConnector.setRequestProperty("Content-Type", "application/json");
+                    httpConnector.setRequestProperty("Accept", "application/json");
+
                     httpConnector.connect();
 
-                    resCode = httpConnector.getResponseCode();
-
-                    outputServer = new PrintStream(httpConnector.getOutputStream());
+                    //JSONをString型で送信
+                    outputServer = new PrintWriter(httpConnector.getOutputStream());
+//                    outputServer = new PrintStream(httpConnector.getOutputStream());
                     outputServer.print(getUserJson(userName, guardianMail));
-                }catch(MalformedURLException e){
-                    //System.out.println("URLが不正です");
-                }catch(IOException e){
-                    //System.out.println("接続失敗");
-                }finally{
-                    if(outputServer != null) outputServer.close();
-                    if(httpConnector != null) httpConnector.disconnect();
+                    outputServer.flush();
+
+                    resCode = httpConnector.getResponseCode();
+                } catch (MalformedURLException e) {
+                    Log.e("例外発生", "URLが不正です", e);
+                } catch (IOException e) {
+                    Log.e("例外発生", "接続失敗", e);
+                } finally {
+                    if (outputServer != null) outputServer.close();
+                    if (httpConnector != null) httpConnector.disconnect();
                 }
 
-                return new Integer(resCode);
-            }
+                Log.d("ResCode", "HTTP通信で返ってきた値は" + resCode);
 
-            @Override
-            protected void onPostExecute(Integer resCode){
+
                 //TODO:既存、登録、その他の3種類くらいで、条件分けする予定
-                switch(resCode){
+                switch (resCode) {
                     case HttpURLConnection.HTTP_OK:
                         postResStr = "OK!新規アカウント登録に成功しました";
                         postResCode = true;
+                        break;
+                    case 404:
+                        postResStr = "ページが存在しない";
                         break;
                     default:
                         postResStr = "NG!新規アカウント登録に失敗しました";
                         postResCode = false;
                 }
+
+                return new Boolean(postResCode);
             }
-        };
 
-        test.execute();
 
-        return postResCode;
+            @Override
+            protected void onPostExecute(Boolean isResCode) {
+                Log.d("HTTP", "onPostExecute通過 => " + postResStr);
+                if (isResCode == false) {
+                    return;
+                }
+
+                loginAccount();
+
+                Log.d("End", "終了！！");
+            }
+
+        }.execute();
+
     }
 
     /** ユーザ名と保護者メールから、JSON形式のString型を作成して返す */
     private String getUserJson(String userName, String guardianMail){
-        return "[{userName:" + userName + ", guardianMail:" + guardianMail + "}]";
+        return "{\"userName\":\"" + userName + "\", \"guardianAdd\":\"" + guardianMail + "\"}";
+        //return "{userName:" + userName + ", guardianAdd:" + guardianMail + "}";
     }
 
 
     /** ログインすると同時に、アカウント情報をアプリに記憶させる */
-    public void loginAccount(Context context, String userName, String guardianMail){
-        setUserName(userName);
-        setGuardianMail(guardianMail);
-
-        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+    public void loginAccount(){
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(accountCreateContext).edit();
         editor.putString(USER_NAME_KEY, userName);
         editor.putString(GUARDIAN_MAIL_KEY, guardianMail);
         editor.commit();
