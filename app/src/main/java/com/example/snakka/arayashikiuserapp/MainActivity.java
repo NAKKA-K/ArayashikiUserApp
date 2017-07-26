8800cbe86de0f5cffb23bd35bc4e3292c59cfdd0
0831 <kd1243444@st.kobedenshi.ac.jp>
2017-07-26 15:34:54 +0900

    お願いします・・・・これで成功してください・・・・・・

--- app/src/main/java/com/example/snakka/arayashikiuserapp/MainActivity.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         bleMgr = new BLEManager(this);
         if(bleMgr.isBleSupport() == false){
-            this.moveTaskToBack(true);
+            this.finishAndRemoveTask();
         }
 
     }
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
      *始めてアプリを起動したときはアカウント登録画面に飛ぶ。*/
     public void onStartButtonClick(View view){
         //TODO:テスト実装。現在の作業はすべて音声案内画面で行われるはずなため、常にtrueになるように設定
-        if(true || AccountManager.loginedAccount(view.getContext()) == true){
+        if(AccountManager.loginedAccount(view.getContext()) == true){
             Intent intent = new Intent(MainActivity.this, VoiceGuideActivity.class);
             startActivity(intent);
 
