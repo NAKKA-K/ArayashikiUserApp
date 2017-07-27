a1219119384d352d2c452b26b89b7dbf180f7788
0831 <kd1243444@st.kobedenshi.ac.jp>
2017-07-27 16:57:29 +0900

    リファクタリング完了～

--- app/src/main/java/com/example/snakka/arayashikiuserapp/VoiceRevival.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/VoiceRevival.java
@@ -6,10 +6,15 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
+import android.view.View;
+
 import static com.example.snakka.arayashikiuserapp.SensorNumber.END;
 import static com.example.snakka.arayashikiuserapp.SensorNumber.FRONT;
 import static com.example.snakka.arayashikiuserapp.SensorNumber.LEFT;
 import static com.example.snakka.arayashikiuserapp.SensorNumber.RIGHT;
+import static com.example.snakka.arayashikiuserapp.VoiceGuideActivity.reVoice;
+import static com.example.snakka.arayashikiuserapp.VoiceGuideActivity.textView1;
+import static com.example.snakka.arayashikiuserapp.VoiceGuideActivity.textView2;
 
 public class VoiceRevival {
     //音声の属性を指定するクラス
@@ -39,6 +44,19 @@ public class VoiceRevival {
     private int[] directionNums = new int[4];
     //読み込みをした際に受け取る音声Id配列
     public static final int[] voiceIds = new int[6];
+    //音声ファイル読み込みの可否を格納する配列
+    private boolean loadSuccessd[] =
+            {
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+            };
+    // 読み込みが成功する度に1ずつ加算していく値
+    // 読み込みが完了しているかどうかをチェックする関数内で
+    // 上記の読み込み成否を格納する配列の添え字に使用する
+    private int loadSuccessIdx = 0;
     // TextViewに表示する文字列を格納変数(後に戻り値となる)
     public String viewString = "";
 
@@ -69,6 +87,48 @@ public class VoiceRevival {
         //音声をロード
         for (int i = 0; i < voiceResources.length; i++)
             voiceIds[i] = soundPool.load(context, voiceResources[i], 1);
+    }
+
+    //音声再生の全てを司るメソッド
+    public void mainVoice() {
+        //行ける方向の文字列を返す(先に同期処理を終わらせてしまう)
+        viewString = viewVoice();
+        // 返ってきたviewStringの中身が空のままだったら行き止まりと判定
+        if (viewString.isEmpty()) {
+            textView2.setText("行き止まりです");
+        } else {
+            textView1.setText(viewString);
+            textView2.setText("に進めます");
+        }
+
+        // 読み込みが終わったか確認する関数
+        // 読み込みが非同期で行われているためか、
+        // この関数も非同期で行われるっぽい
+        // 1読み込みにつき1回この関数が呼ばれる
+        // そのため同じ音声に関しては1度読み込まれれば解放→再度読み込みとしない限り呼ばれることはない(byよーすけさん)
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int soundId, int status) {
+                // 読み込みが成功している(statusが0)なら
+                //startVoiceメソッド内に同期処理を記述
+                if (status == 0) {
+                    loadSuccessd[loadSuccessIdx++] = true;
+                    // 無事全部読み込めてたら
+                    if (loadSuccessd[0] && loadSuccessd[1] && loadSuccessd[2] && loadSuccessd[3] && loadSuccessd[4]) {
+                        //音声再生
+                        startVoice();
+                    }
+                }
+            }
+        });
+
+        //ボタンをクリックすると音声再生
+        reVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoice();
+            }
+        });
     }
 
     //音声を再生するメソッド(非同期処理はVoiceGuideActivity内で行われる)
