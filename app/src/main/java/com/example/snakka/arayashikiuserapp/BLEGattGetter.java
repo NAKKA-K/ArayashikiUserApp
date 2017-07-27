29c8237c3464df44acf362b6d94c797da95a2b6d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-07-27 18:10:16 +0900

    内部の値を取得

--- app/src/main/java/com/example/snakka/arayashikiuserapp/BLEGattGetter.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/BLEGattGetter.java
@@ -4,10 +4,14 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BLEGattGetter {
     private static BluetoothGattCallback bleGattCallback;
@@ -41,50 +45,38 @@ public class BLEGattGetter {
             public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
                 super.onConnectionStateChange(gatt, status, newState);
 
-                if (newState != BluetoothProfile.STATE_CONNECTED) return;
+                if (newState == BluetoothProfile.STATE_DISCONNECTED){
+                    gatt.close();
+                    return;
+                }
 
-                //bleGatt = gatt; //こういう記述があったが必要なのか？
+                Log.e("discoverServices()", "GATTに接続");
                 bleGatt.discoverServices();
             }
 
-            /**
-             * Callback invoked when the list of remote services, characteristics and descriptors
-             * for the remote device have been updated, ie new services have been discovered.
-             *
-             * @param gatt   GATT client invoked {@link BluetoothGatt#discoverServices}
-             * @param status {@link BluetoothGatt#GATT_SUCCESS} if the remote device
-             */
+
             @Override
             public void onServicesDiscovered(BluetoothGatt gatt, int status) {
                 super.onServicesDiscovered(gatt, status);
 
-                if(status != BluetoothGatt.GATT_SUCCESS || bleGatt.getService(UUID.fromString(SENSOR_UUID)) == null) return;
+                if(status != BluetoothGatt.GATT_SUCCESS) return;
 
-                //サービスを取得->キャクタリスティクスを取得 ==> 読み込み開始(非同期実行)
-                BluetoothGattCharacteristic characteristic
-                        = bleGatt.getService( UUID.fromString(SENSOR_UUID) )
-                        .getCharacteristic( UUID.fromString(SENSOR_UUID) );
-
-                bleGatt.readCharacteristic(characteristic); //読み込めると、同じクラス内のonCharacteristicReadが呼ばれる。(非同期)
+                //デバイスのサービスからabcdのUUIDを持つ物を探し、内部のキャラクタリスティックを取得
+                for(BluetoothGattService service: bleGatt.getServices()){
+                    if(isArayashikiUUID(service.getUuid().toString())){
+                        bleGatt.readCharacteristic(service.getCharacteristics().get(0)); //読み込めると、同じクラス内のonCharacteristicReadが呼ばれる。(非同期)
+                    }
+                }
             }
 
 
-            /**
-             * Callback reporting the result of a characteristic read operation.
-             *
-             * @param gatt           GATT client invoked {@link BluetoothGatt#readCharacteristic}
-             * @param characteristic Characteristic that was read from the associated
-             *                       remote device.
-             * @param status         {@link BluetoothGatt#GATT_SUCCESS} if the read operation
-             */
+
             @Override
             public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
                 super.onCharacteristicRead(gatt, characteristic, status);
 
                 if(status != BluetoothGatt.GATT_SUCCESS) return;
 
-
-                //TODO:戻り値をint型に変換してやる必要あり
                 sensorNum = characteristic.getValue();
 
                 closeGatt();
@@ -92,22 +84,38 @@ public class BLEGattGetter {
         };
     }
 
+    /** arayashiki内のabcdを持つUUID文字列を選択する */
+    public boolean isArayashikiUUID(String uuid){
+        Pattern pattern = Pattern.compile("^0000abcd"); //これ以外の文字が含まれると弾く
+        Matcher matcher = pattern.matcher(uuid);
+
+        if(matcher.find()){
+            return true;
+        }
+        return false;
+    }
+
+
 
     /** deviceに接続すると同時に、データの取得フラグをoffにする */
     public void connectGatt(Context context, BluetoothDevice device){
+        if(device == null) return;
+
         setIsGattGot(false);
         bleGatt = device.connectGatt(context, false, bleGattCallback);
         bleGatt.connect();
     }
 
     public void closeGatt(){
-        bleGatt.close();
+        if(bleGatt == null) return;
+
         setIsGattGot(true);
+        bleGatt.disconnect();
     }
 
 
-    /** Activityがpouseしたときに呼ぶべき処理 */
-    public void pauseGattGetter(){
+    /** Activityがcancelしたときに呼ぶべき処理 */
+    public void cancelGattGetter(){
         closeGatt();
     }
 
