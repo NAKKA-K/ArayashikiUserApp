a003ba9d66d4f82ce48ebfb30f718dd5019b6be4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-07-27 19:23:13 +0900

    整理

--- app/src/main/java/com/example/snakka/arayashikiuserapp/BLEScanner.java
+++ app/src/main/java/com/example/snakka/arayashikiuserapp/BLEScanner.java
@@ -4,12 +4,8 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
-import android.os.ParcelUuid;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class BLEScanner {
     private static BluetoothLeScanner bleLeScanner;
@@ -17,11 +13,9 @@ public class BLEScanner {
     private static BluetoothDevice sensorDevice;
     private boolean isScanning = false;
 
-    private static String SENSOR_UUID;
 
-    public BLEScanner(BluetoothLeScanner bleLeScanner, String uuid){
+    public BLEScanner(BluetoothLeScanner bleLeScanner){
         this.bleLeScanner = bleLeScanner;
-        this.SENSOR_UUID = uuid;
 
         scanCallback = initScanCallback();
     }
@@ -30,38 +24,20 @@ public class BLEScanner {
     /** scanCallback変数を初期化するメソッド */
     private ScanCallback initScanCallback(){
         return new ScanCallback() {
-            /**
-             * BLE端末が見つかった場合のコールバック。
-             * @param callbackType このコールバックがどのように発動されたのか決定する。どれか1つの可能性があります。
-             *                     {ScanSettings#CALLBACK_TYPE_ALL_MATCHES}
-             *                     {ScanSettings#CALLBACK_TYPE_FIRST_MATCH}
-             *                     {ScanSettings#CALLBACK_TYPE_MATCH_LOST}
-             * @param result       BLEで受信した結果オブジェクト。
-             */
             @Override
             public void onScanResult(int callbackType, ScanResult result) {
                 super.onScanResult(callbackType, result);
 
-                //TODO:指定のデバイスが見つかったら代入
                 BluetoothDevice device = result.getDevice();
-                Log.d("デバイス探索", "デバイスじゃ"+device.getName());
-                if(isSensorDevice(device.getName()) == false || isEqualDevice(device)) return; //センサーで無いか同じセンサであれば用無し
+
+                //センサーで無いか、同じセンサであれば用無し
+                if(isSensorDevice(device.getName()) == false || isEqualDevice(device)) return;
 
                 setSensorDevice(device);
-
-                Log.e("ScanDevice name", ""+device.getName());
-                Log.e("ScanDevice getName", ""+result.getScanRecord().getDeviceName()); //取得したデバイスの名前
+                Log.e("ScanCallback()", "取得したデバイス名 = " + device.getName());
 
                 stopScanDevice();
             }
-
-
-            /* バッチ結果が配信されるときのコールバック。onBatchScanResults(List<ScanResult> results)
-             * @param results 以前にスキャンされた結果のリスト。
-             */
-            /*スキャンを開始できなかったときのコール。onScanFailed(int errorCode)
-             *@param errorCode スキャン失敗したときのエラーコード(SCAN_FAILED_* の中の1つ)
-             */
         };
     }
 
@@ -69,12 +45,9 @@ public class BLEScanner {
 
     /** 保存されているセンサーと検出したセンサーが同じならtrue */
     public boolean isEqualDevice(BluetoothDevice device){
-/*
         if (this.sensorDevice != null && this.sensorDevice == device){
             return true;
-        }else{
-            return false;
-        }*/
+        }
         return false;
     }
 
@@ -92,7 +65,7 @@ public class BLEScanner {
     public void startScanDevice(){
         if(isScanning == true) return;
 
-        Log.d("startScanDevice", "デバイススキャン開始");
+        Log.d("startScanDevice()", "デバイススキャン開始");
         setIsScanning(true);
         bleLeScanner.startScan(scanCallback);
     }
